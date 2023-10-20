4bc5a445bc47cc90a985d2475ab6e398c4216ecd
vmarasinskiy <vmarasinskiy@gmail.com>
2023-10-20 09:40:34 +0300

    add file README.md, update calculations.js_2.2.4

--- calculations.js
+++ calculations.js
@@ -23,3 +23,4 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
+
