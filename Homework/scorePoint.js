7e21737de9aa14fc51e9aca4f5a00a7cc25b9f3a
vmarasinskiy <vmarasinskiy@gmail.com>
2023-10-24 13:18:55 +0300

    add file scorePoint_2.3.1.1

---
+++ Homework/scorePoint.js
@@ -0,0 +1,34 @@
+a=[]
+
+const players = [
+{
+id: 1,
+name: 'Ivan',
+scorePoints: 4500
+},
+{
+id: 2,
+name: 'Petr',
+scorePoints: 3600
+},
+{
+id: 3,
+name: 'Vadim',
+scorePoints: 3433
+},
+{
+id: 4,
+name: 'Olga',
+scorePoints: 2356
+},
+
+];
+
+for(i in players) {
+    a.push(players[i]['scorePoints'])//добавление элементов в пустой массив
+}
+
+Maximum=Math.max(...a)//ищем максимальное значение в НОВОМ массиве а
+ID_Maximum=a.indexOf(Maximum);//ищем индекс максимального значения в массиве a
+result_name=players[ID_Maximum]['name'];// ищем имя в массиве players, индекс которого совпадает с индексом максимального значения из массива a
+console.log(Maximum, result_name);
