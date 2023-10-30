dc1d937cc4ebcce6d9d967345836c6edfabee102
vmarasinskiy <vmarasinskiy@gmail.com>
2023-10-30 14:24:43 +0300

    update file class 2.3.2

--- Class/class.js
+++ Class/class.js
@@ -1,25 +1,34 @@
 class Button {
-    constructor(width, height) {
-        this.props = {
-            width: width,
-            height: height,
-            type: 'button',
-            color: 'green'
-        };
-        console.log(this.props)
-    }
-    
-    testButton(x, y) {
-    if (this.props.width == x && this.props.height == y) 
-    {
-      console.log("Свойства соответствуют ожидаемым значениям.");
-    } 
-    else {
-      console.log("Свойства не соответствуют ожидаемым значениям.");
-        }
-    } 
+  constructor(width, height, color) {
+    this.props = {
+      width: width,
+      height: height,
+      type: 'button',
+      color: color
+    };
+  }
 
+  onClick() {
+    console.log({
+      Width: this.props.width,   
+      Height: this.props.height,
+      Type: this.props.type,     
+      Colour: this.props.color   
+    });
+  }
+
+  testButton(x, y, z) {
+    if (this.props.width === x && this.props.height === y && this.props.color===z) {
+      console.log("Свойства соответствуют ожидаемым значениям.");
+    } else {
+      console.log("Свойства не соответствуют ожидаемым значениям.");
+    }
+  }
 }
 
-const test = new Button(42, 50)
-test.testButton(42, 50);
+const test1 = new Button(42, 50, 'red');
+const test2 = new Button(43, 50, 'green');
+test1.onClick();
+test2.onClick();
+test1.testButton(42, 50, 'red');
+test2.testButton(42, 50, 'red');
