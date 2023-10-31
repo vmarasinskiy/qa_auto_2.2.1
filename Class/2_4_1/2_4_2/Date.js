b5a606a39a1f9076bb2b66101754bc31b3d0b1a3
vmarasinskiy <vmarasinskiy@gmail.com>
2023-10-31 14:16:28 +0300

    add file date 2_4_2

---
+++ Class/2_4_1/2_4_2/Date.js
@@ -0,0 +1,21 @@
+let expencesExamples = [
+    { yearlyExpences: [2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390] },
+    { yearlyExpences: [500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200] },
+    { yearlyExpences: [20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000] },
+   ];
+
+   function getMonthName(monthIndex) {
+    const months = [
+        "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+        "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+    ];
+    return months[monthIndex];
+
+   }
+ 
+   expencesExamples[1]["yearlyExpences"].forEach(function(value, index) {
+    if (value <= 1000) {
+                const monthName = getMonthName(index);
+                console.log(`element ${value}, Date: ${monthName}`);
+    }
+});
