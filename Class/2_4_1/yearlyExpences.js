f5cc655354fc7d85e32b8f1d3355f68f7015e04a
vmarasinskiy <vmarasinskiy@gmail.com>
2023-10-31 10:20:27 +0300

    add file yearlyExpences_2.4.1

---
+++ Class/2_4_1/yearlyExpences.js
@@ -0,0 +1,12 @@
+let expencesExamples = [
+    { yearlyExpences: [2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390] },
+    { yearlyExpences: [500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200] },
+    { yearlyExpences: [20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000] },
+   ];
+
+
+   expencesExamples[1]["yearlyExpences"].forEach(function(value, index) {
+    if (value > 1000) {
+        console.log(`element ${value}, index: ${index}`);
+    }
+});
