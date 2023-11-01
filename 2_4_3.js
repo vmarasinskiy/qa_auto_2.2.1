f4bab347569deda30b598c3904e6966d0b93bbb9
vmarasinskiy <vmarasinskiy@gmail.com>
2023-11-01 15:10:29 +0300

    update file_2.4.4

---
+++ 2_4_3.js
@@ -0,0 +1,10 @@
+const calculateBonus = (a, b) => {
+    let bonus;
+    const sum = a + b;
+    debugger;
+    sum > 50 ? (bonus = 50) : (bonus = sum);
+    debugger;
+    return bonus;
+    };
+
+    console.log(calculateBonus);
