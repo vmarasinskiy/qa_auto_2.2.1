4e8bd8fcd62c67d4a6b613efa0943e104b26d1f8
vmarasinskiy <vmarasinskiy@gmail.com>
2023-11-02 11:13:37 +0300

    update file Functional_2.3.3

--- Functional task/Functional.js
+++ Functional task/Functional.js
@@ -1,10 +1,17 @@
 let userBalance = 500 // Сумма на счете пользователя
 let shippingCost = 100 // Стоимость посылки
-let smsCode = "A001DFX0" // Код из смс
+let idPost = "1432HGF" // ID посылки
+let client_data_code = "A001DFX0" // Код из смс
+const parcel_data_code = "A001DFX0"
 let postBoxState = [null, null, null, "1432HGF", null] // Почтамат с ячейками
 
-if (smsCode == "A001DFX0") {
-    console.log("Заберите посылку 1432HGF из ячейки №4. Ваш счет составляет: 400 ед.");
+if (postBoxState[3] == idPost) {
+    if (parcel_data_code == client_data_code) {
+        userBalance = userBalance-shippingCost; // Уменьшаем баланс пользователя на стоимость доставки
+        console.log("Заберите посылку 1432HGF из ячейки №4. Ваш счет составляет: " + userBalance + " ед.");
+    } else {
+        console.log('Указан неверный код');
+    }
 } else {
-    console.log("Посылка не найдена в почтамате.");
+    console.log('Посылка к сожалению не найдена в почтамате.');
 }
